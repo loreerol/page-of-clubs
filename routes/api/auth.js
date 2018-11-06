ef422e7cc25efaa07117b48c30d385b48e40d9b6
loreerol <lorehill.hello@gmail.com>
2018-11-06 13:40:27 -0500

    Set up a protected login route that returns data of user who logged in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
-const passport = require('passport');
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load User model
 const User = require("../../models/User");
@@ -59,16 +59,20 @@ router.post("/login", (req, res) => {
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
         // User matched
-
-          const payload =  { name: user.name }
+        const payload = { id: user.id, name: user.name };
 
         // Sign Token
-        jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600 }, (err, token) => {
-          res.json({
-            success: true,
-            token: 'Bearer ' + token
-          });
-        });
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
@@ -79,8 +83,15 @@ router.post("/login", (req, res) => {
 // @route   GET api/auth/current
 // @desc    Return current user
 // @access  Private
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-  res.json({msg: 'success'});
-});
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(
+      {id: req.user.id,
+      name: req.user.name,}
+    );
+  }
+);
 
 module.exports = router;
