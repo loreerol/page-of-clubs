6e2952c3e8fad56253a4e62f45c36aeccb826381
loreerol <lorehill.hello@gmail.com>
2018-11-22 10:12:30 -0500

    created seperate files for redux store and reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-const store = createStore(() => [], {}, applyMiddleware());
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
 export default store;
